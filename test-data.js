3912f478141d8e08b1d2c7ea1a4bb633d4099a35
raffansmeksapur-tech <raffansmeksapur@gmail.com>
2025-11-20 20:18:28 +0700

    Update test-data.js

--- test-data.js
+++ test-data.js
@@ -3,16 +3,16 @@ const TOTAL_TESTS = 10;
 
 // Test plates data
 const testPlates = [
-    { number: 12, image: 'images/plate-01.png' },
-    { number: 8,  image: 'images/plate-02.png' },
-    { number: 6,  image: 'images/plate-03.png' },
-    { number: 29, image: 'images/plate-04.png' },
-    { number: 5,  image: 'images/plate-05.png' },
-    { number: 3,  image: 'images/plate-06.png' },
-    { number: 15, image: 'images/plate-07.png' },
-    { number: 74, image: 'images/plate-08.png' },
-    { number: 2,  image: 'images/plate-09.png' },
-    { number: 16, image: 'images/plate-10.png' }
+    { number: 26, image: 'images/plate-01.png' },
+    { number: 13,  image: 'images/plate-02.png' },
+    { number: 16,  image: 'images/plate-03.png' },
+    { number: 7, image: 'images/plate-04.png' },
+    { number: 45,  image: 'images/plate-05.png' },
+    { number: 6,  image: 'images/plate-06.png' },
+    { number: 74, image: 'images/plate-07.png' },
+    { number: 15, image: 'images/plate-08.png' },
+    { number: 29,  image: 'images/plate-09.png' },
+    { number: 3, image: 'images/plate-10.png' }
 ];
 
 // Test state
